dfb3e9d142a0d3f22c849dca96d60c0312cbc784
JonoAlejandro <jonathan.moran1999@gmail.com>
2021-08-28 13:45:21 +1000

    Added Timer Script (NEEDS WORK)

---
+++ Project Boost - Unity Udemy 2 NEW/Assets/Scripts/Menu Scripts/timer.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class timer : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    float time;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.time;
+        int timeInt = (int)time;
+        timerText.text = "Timer: " + timeInt;
+    }
+}
