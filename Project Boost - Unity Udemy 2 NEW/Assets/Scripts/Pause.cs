aab6d890562918944ded64a4f0881f236953d5c6
JonoAlejandro <jonathan.moran1999@gmail.com>
2021-08-29 15:06:44 +1000

    Fixed Pausing and Unpausing Bugs
    - Keeps Momentum when paused so ship doesnt fall down after unpause.
    
    - Destroys pause menu when in main menu.

--- Project Boost - Unity Udemy 2 NEW/Assets/Scripts/Pause.cs
+++ Project Boost - Unity Udemy 2 NEW/Assets/Scripts/Pause.cs
@@ -8,66 +8,74 @@ public class Pause : MonoBehaviour
 {
 
     [SerializeField] GameObject mainMenu;
-
     // Components
-    Mover moverScript;
-    Rigidbody rb;
 
     //States
-    bool pause = false;
+    bool pauseToggle = false;
 
 
     // Start is called before the first frame update
     void Start()
     {
         mainMenu.SetActive(false);
-        GrabCompenents();
+
     }
 
-    void GrabCompenents()
-    {
-        moverScript = GetComponent<Mover>();
-        rb = GetComponent<Rigidbody>();
-    }
 
     // Update is called once per frame
     void Update()
     {
         PauseGame();
-        UnpauseGame();
+        FirstSceneCheck();
+    }
+
+    void FirstSceneCheck()
+    {
+        //checking if current scene is main menu
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        if (currentScene == 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("deleted Pause Screen");
+            Time.timeScale = 1;
+        }
     }
 
     void PauseGame()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {   
-            Debug.Log("Going to Pause Menu");
-            Cursor.visible = true;
-            mainMenu.SetActive(true);
-            moverScript.enabled = false;
-            rb.constraints = RigidbodyConstraints.FreezeAll;
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseToggle = !pauseToggle;
+            Debug.Log("Pause is " + pauseToggle);
         }
 
-
-    }
-
-    void UnpauseGame() 
-    {
-        if (Input.GetKeyDown(KeyCode.U) || pause)
+        
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseToggle)
+        {
+            PauseSequence();
+        }
+        if (Input.GetKeyDown(KeyCode.Escape) && !pauseToggle)
         {
             UnpauseSequence();
         }
+
+    }
+
+    void PauseSequence()
+    {
+        Time.timeScale = 0;
+        Debug.Log("Going to Pause Menu");
+        Cursor.visible = true;
+        mainMenu.SetActive(true);
     }
 
     public void UnpauseSequence()
     {   
-        Debug.Log("here");
-        Cursor.visible = false;
         mainMenu.SetActive(false);
-        rb.freezeRotation = false;
-        moverScript.enabled = true;
-        rb.constraints = RigidbodyConstraints.None;
-        
+        Time.timeScale = 1;
+        Debug.Log("Unpausing Game");
+        Cursor.visible = false; 
     }
 
     public void GoToMainMenu()
